f5a504b9ebee82b7ba7efc77f8cfa02663afacaa
Fabiano Del Villar <fabianodelvillar@gmail.com>
2026-06-09 13:36:45 -0500

    refactor: remove unused SupportedCurrenciesResponse record

--- src/main/java/com/rivaldo/currencyexchange/entity/supportedcodes/SupportedCurrenciesResponse.java
+++
@@ -1,9 +0,0 @@
-package com.rivaldo.currencyexchange.entity.supportedcodes;
-
-import java.util.List;
-
-public record SupportedCurrenciesResponse(
-        List<CurrencyCodeDto> currencies,
-        int totalCurrencies
-) {
-}
